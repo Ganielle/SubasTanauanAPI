0b098bef10e417c098a517fb711caf93d2d03e9b
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-12-13 08:48:54 +0800

    Update cashin.js

--- controllers/cashin.js
+++ controllers/cashin.js
@@ -47,8 +47,132 @@ exports.requestcashin = async (req, res) => {
     return res.json({message: "success"})
 }
 
-exports.cashinrequestlist = async (req, res) => {
+//  #endregion
 
+//  #region SUPERADMIN
+
+
+exports.cashinrequestlist = async (req, res) => {
+    const {id, username} = req.user
+
+    const {page, limit, searchname, status} = req.query
+
+    if (!status){
+        return res.status(400).json({message: "failed", data: "Please select a valid status first!"})
+    }
+    else if (status != "Approved" && status != "Denied"){
+        return res.status(400).json({message: "failed", data: "Please select between approved and denied only"})
+    }
+
+    const pageOptions = {
+        page: parseInt(page) || 0,
+        limit: parseInt(limit) || 10
+    }
+
+    const matchStage = {}
+    if (searchname){
+        searchname.$or = [
+            { 'details.firstname': { $regex: searchname, $options: 'i' } },
+            { 'details.lastname': { $regex: searchname, $options: 'i' } },
+            { $expr: { $regexMatch: { input: { $concat: ['$details.firstname', ' ', '$details.lastname'] }, regex: searchname, options: 'i' } } } // Search for first + last name
+        ];
+    }
+
+    const result = await Cashin.aggregate([
+        {
+            $match: {
+                status: status
+            }
+        },
+        {
+            $lookup: {
+                from: 'userdetails', // Collection name for the 'userDetails' schema
+                localField: 'owner',
+                foreignField: 'owner',
+                as: 'details'
+            }
+        },
+        {
+            $unwind: '$details' // Deconstruct the 'details' array to a single object
+        },
+        {
+            $match: matchStage // Apply the dynamic auth filter if provided
+        },
+        {
+            $project: {
+                $_id: 0,
+                cashinid: "$_id",
+                fullname: { $concat: ['$details.firstname', ' ', '$details.lastname']},
+                amount: 1,
+                paymentmethod: 1,
+                accountnumber: 1,
+                accountname: 1,
+                receipt: 1,
+                status: 1
+            }
+        },
+        { $skip: pageOptions.page * pageOptions.limit },
+        { $limit: pageOptions.limit }
+    ])
+
+    const total = await Cashin.aggregate([
+        {
+            $match: {
+                status: status
+            }
+        },
+        {
+            $lookup: {
+                from: 'userdetails', // Collection name for the 'userDetails' schema
+                localField: 'owner',
+                foreignField: 'owner',
+                as: 'details'
+            }
+        },
+        {
+            $unwind: '$details' // Deconstruct the 'details' array to a single object
+        },
+        {
+            $match: matchStage // Apply the dynamic auth filter if provided
+        },
+        { $count: 'total' }
+    ]);
+
+    const totalPages = Math.ceil((total[0]?.total || 0) / pageOptions.limit);
+
+    const data = {
+        list: result,
+        totalpages: totalPages
+    };
+
+    return res.json({message: "success", data: data})
+}
+
+exports.approvedeniecashinrequest = async (req, res) => {
+    const {id, username} = req.user
+
+    const {requestid, status} = req.body
+
+    if (!requestid){
+        return res.status(400).json({message: "Please select a valid cashin request"})
+    }
+    else if (!status){
+        return res.status(400).json({message: "Please select a valid status"})
+    }
+    else if (status != "Approved" && status != "Denied"){
+        return res.status(400).json({message: "Please select between Approved and Denied only"})
+    }
+
+    const cashinrequest = await Cashin.findOneAndUpdate({_id: new mongoose.Types.ObjectId(requestid)}, {status: status})
+    .catch(err => {
+        console.log(`There's a problem with ${status} cashin request for the id ${requestid}. Error: ${err}`)
+
+        return res.status(400).json({message: "bad-request", data: "There's a problem with the server! Please try again later"})
+    })
+
+    if (status == "Approved"){
+        await Wallets.findOneAndUpdate()
+    }
 }
 
 //  #endregion
